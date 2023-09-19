1efc5e9120a7d48e7a64c2e4d624116997ce72f0
Mamun128169 <mh4559641@gmail.com>
2023-09-19 12:33:50 +0600

    add blogs.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header'
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
